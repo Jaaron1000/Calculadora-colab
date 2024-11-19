c9763a106da25c4f5f131306d6e486690282c10a
Aaron Sanchez <jose2006jose@gmail.com>
2024-11-19 10:58:18 +0100

    añadiendo la calculadora

--- Calculadora.java
+++ Calculadora.java
@@ -12,6 +12,27 @@ public class Calculadora {
                 2. Restar
                 3. Multiplicar
                 4. Dividir""");
+        double resultado = 0;
         int accion = new Scanner(System.in).nextInt();
+        if(accion==1){
+            resultado = n1+n2;
+            System.out.println("El resultado es: "+resultado);
+        }else if(accion==2){
+            resultado = n1-n2;
+            System.out.println("El resultado es: "+resultado);
+        }else if(accion==3){
+            resultado = n1*n2;
+            System.out.println("El resultado es: "+resultado);
+        }else if(accion==4){
+            if(n2!=0){
+                resultado = n1/n2*1.0;
+                System.out.println("El resultado es: "+resultado);
+            }else {
+                System.out.println("El divisor no puede ser 0");
+            }
+        }else{
+            System.out.println("La seleccion introducda no es valida");
+        }
+
     }
 }
